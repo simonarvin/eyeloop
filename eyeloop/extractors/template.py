e6c45c607d074cc7f6a8ecffa2c6f0061ac5cd7d
Simon Arvin <40764374+simonarvin@users.noreply.github.com>
2020-11-13 15:27:04 +0100

    extractor template added

---
+++ eyeloop/extractors/template.py
@@ -0,0 +1,18 @@
+import time
+
+import cv2
+import numpy as np
+
+
+class Template_extractor():
+    def __init__(self, x: int = 50, y: int = 50, w: int = 50, h: int = 50) -> None:
+        self.x, self.y, self.width, self.height = x, y, w, h
+
+    def activate(self) -> None:
+        return
+
+    def release(self):
+        return
+
+    def fetch(self, engine):
+        return
