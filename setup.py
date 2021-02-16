04ed9d0e23c4442dc3cecc7d42c9534efd068a23
Simon Arvin <sarv@dandrite.au.dk>
2021-02-16 15:45:52 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     license_file='LICENSE',
     platforms='any',
     python_requires='>=3.7',
-    version='0.2',
+    version='0.3',
     entry_points={
         'console_scripts': [
             'eyeloop=eyeloop.run_eyeloop:main'
